3c5009c2df22244abcf22ad4886cc99b44fe3aef
Vinicius Prudencio <vynnyprudencio@gmail.com>
2023-07-04 17:02:10 -0300

    feat: add store prisma schema and create store method using mysql with planetscale

--- packages/frontend/ecommerce-admin/app/layout.tsx
+++ packages/frontend/ecommerce-admin/app/layout.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Inter } from 'next/font/google'
 
 import { ClerkProvider } from '@clerk/nextjs'
+import { ToastProvider } from '@/providers/toast-provider'
 import { ModalProvider } from '@/providers/modal-provider'
 
 import './globals.css'
@@ -22,6 +23,7 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
+          <ToastProvider />
           <ModalProvider />
           {children}
         </body>

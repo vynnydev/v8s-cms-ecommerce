724315083df57fab20709c49ffa8a2cb8bb71a80
Vinicius Prudencio <vynnyprudencio@gmail.com>
2023-07-04 13:59:07 -0300

    feat: add complete clerk authentication

--- packages/frontend/ecommerce-admin/app/(root)/page.tsx
+++ packages/frontend/ecommerce-admin/app/(root)/page.tsx
@@ -1,9 +1,9 @@
-import { Button } from '@/components/ui/button'
+import { UserButton } from '@clerk/nextjs'
 
 const SetupPage = () => {
   return (
     <div className="p-4">
-      <Button>Click Me</Button>
+      <UserButton afterSignOutUrl="/" />
     </div>
   )
 }
